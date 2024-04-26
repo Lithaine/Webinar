2f2e26d2f7b7edb1f2254f383fc9a885a54c6d2c
Lithaine <168147085+Lithaine@users.noreply.github.com>
2024-04-26 16:39:35 +0800

    Add files via upload

---
+++ num.js
@@ -0,0 +1,24 @@
+let num1 = 5;
+let num2 = 2;
+let num3 = 10;
+
+function sum(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+console.log("Sum of three numbers: " + sum(num1, num2, num3));
+
+function difference(num3, num2) {
+  return num3 - num2;
+}
+console.log("Difference of two numbers: " + difference(num3, num2));
+
+function product(num1, num2) {
+  return num1 * num2;
+}
+console.log("Product of two numbers: " + product(num1, num2));
+
+
+function average(num3, num1){
+    return num3 / num1;
+}
+console.log("Average of two numbers: " + average(num3, num1));
---
+++ script.js
@@ -0,0 +1,9 @@
+function generatePassword() {
+    const length = 8; 
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("generatePassword").value = password;
+}
